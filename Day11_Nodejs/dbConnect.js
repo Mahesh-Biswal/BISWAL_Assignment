5e8e061dee031f8fd3a8df7c63bb7732f0035a57
Mahesh Biswal <mahesh.biswal@merkle.com>
2023-07-24 12:40:38 +0530

    Day11 Assignment

--- Day11_Nodejs/dbConnect.js
+++ Day11_Nodejs/dbConnect.js
@@ -1,43 +1,22 @@
 import mysql from 'mysql2';
-
 class DbConnect {
-
     async connectToDb() {
-
         try {
-
             const connection = await mysql.createConnection({
-
                 host: "localhost",
-
                 user: "root",
-
                 password: "mahesh",
-
                 port: 3067
-
             });
-
             connection.connect(function (err) {
-
                 if (err) throw err;
-
                 console.log("Connected!");
-
             });
-
             return true;
-
         } catch (ex) {
-
             return false;
-
         }
-
     }
-
 }
-
-
 
 export default DbConnect;
